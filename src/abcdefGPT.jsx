d7d7e6155ceeca56fc09b92b15416ce50d95f8fb
방지민 <bjimin7@naver.com>
2023-09-20 02:11:28 +0900

    [Feat] Detach tab and button

--- src/abcdefGPT.jsx
+++ src/abcdefGPT.jsx
@@ -8,14 +8,21 @@ function AbcdefGPT() {
     setIsDivClickable(!isDivClickable);
   };
 
-  const divClassName = isDivClickable ? 'abcdefGPT-result-tab clickable-div white-bg' : 'abcdefGPT-result-tab';
+  const divClassName = isDivClickable ? 'abcdefGPT-result-tab clickable-div white-bg' : 'abcdefGPT-result-tab hidden-div';
 
 
   return (
-    <div className={divClassName}>
+    <div className='custom-tab'>
       <button className='custom-button' onClick={toggleDivClickability}>
         AI 정보 정리
       </button>
+      <div className={divClassName}>
+        <div className='custom-header'>
+        <button className='close-button' onClick={toggleDivClickability}>
+            X
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
