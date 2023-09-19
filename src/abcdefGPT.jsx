ab72155f773a96ec9466d4ddc0cb4c83504311fd
방지민 <bjimin7@naver.com>
2023-09-20 02:28:47 +0900

    [Feat] Mock API Connect

--- src/abcdefGPT.jsx
+++ src/abcdefGPT.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './abcdefGPT.css';
 
 function AbcdefGPT() {
   const [isDivClickable, setIsDivClickable] = useState(false);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const toggleDivClickability = () => {
     setIsDivClickable(!isDivClickable);
@@ -10,6 +12,27 @@ function AbcdefGPT() {
 
   const divClassName = isDivClickable ? 'abcdefGPT-result-tab clickable-div white-bg' : 'abcdefGPT-result-tab hidden-div';
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://f7849c1c-faf4-4fed-b415-610711c64cbd.mock.pstmn.io/apiTest'); 
+        const jsonData = await response.json();
+        if (jsonData.isSuccess) {
+            setData(jsonData.result);
+            console.log(data.str1);
+          } else {
+            console.error('API 요청 실패:', jsonData.message);
+          }
+  
+          setIsLoading(false);
+      } catch (error) {
+        console.error('API 요청 중 오류 발생:', error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className='custom-tab'>
@@ -18,9 +41,17 @@ function AbcdefGPT() {
       </button>
       <div className={divClassName}>
         <div className='custom-header'>
-        <button className='close-button' onClick={toggleDivClickability}>
+            <button className='close-button' onClick={toggleDivClickability}>
             X
-          </button>
+            </button>
+            <div>
+                <h1>데이터를 표시하는 예시</h1>
+                {isLoading ? (
+                <p>데이터를 로드하는 중...</p>
+                ) : (
+                    <p>{data.str1}</p>
+                  )}
+            </div>
         </div>
       </div>
     </div>
